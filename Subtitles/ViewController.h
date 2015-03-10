51d0c09e034fb73386feb0e70b032288f37cae2c
Enie <weiss.enie@gmail.com>
2015-03-10 12:01:49 +0100

    Initial Commit

---
+++ Subtitles/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Subtitles
+//
+//  Created by Enie Weiß on 10/03/15.
+//  Copyright (c) 2015 Enie Studio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ Subtitles/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Subtitles
+//
+//  Created by Enie Weiß on 10/03/15.
+//  Copyright (c) 2015 Enie Studio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
